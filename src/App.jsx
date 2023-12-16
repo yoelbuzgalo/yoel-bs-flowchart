2a8f00b4df5783d622efa4cfe5367c3bbcf140d7
Yoel Buzgalo <yoelbuzgalo@gmail.com>
2023-12-16 14:57:59 -0500

    Initialized Vite

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css';
+import Flowchart from './components/Flowchart';
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1>
+          Yoel&apos;s Bachelors of Science in Software Engineering Degree Plan
+          <Flowchart />
+        </h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Flowchart.jsx
@@ -0,0 +1,12 @@
+import ReactFlow from 'reactflow';
+import 'reactflow/dist/style.css';
+
+function Flowchart() {
+  return (
+    <div>
+      <div className="flowchartContainer"></div>
+    </div>
+  );
+}
+
+export default Flowchart;
